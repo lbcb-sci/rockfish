6c04797e7d83024cb9e5723f649f7fcd03aca2f4
Dominik Stanojevic <dominik.stanojevic@outlook.com>
2021-12-03 07:52:48 +0800

    Fixes

--- rockfish/extraction/extract.py
+++ rockfish/extraction/extract.py
@@ -15,32 +15,42 @@ class Example:
     read_id: str
     ctg: str
     pos: int
-    signal: np.ndarray
+    signal: List[float]
+    cigar: List[int]
     bases: str
 
 
 MotifPositions = dict[str, Tuple[Set[int], Set[int]]]
-def build_reference_idx(aligner: mappy.Aligner, motif: str, 
+
+
+def build_reference_idx(aligner: mappy.Aligner, motif: str,
                         rel_idx: int) -> MotifPositions:
     positions = OrderedDict()
 
     for contig in aligner.seq_names:
         sequence = aligner.seq(contig)
 
-        fwd_pos = {m.start() + rel_idx for m in re.finditer(motif, sequence, re.I)}
+        fwd_pos = {
+            m.start() + rel_idx
+            for m in re.finditer(motif, sequence, re.I)
+        }
 
         rev_comp = mappy.revcomp(sequence)
+
         def pos_for_rev(i: int) -> int:
             return len(sequence) - (i + rel_idx) - 1
-        rev_pos = {pos_for_rev(m.start()) 
-                   for m in re.finditer(motif, rev_comp, re.I)}
+
+        rev_pos = {
+            pos_for_rev(m.start())
+            for m in re.finditer(motif, rev_comp, re.I)
+        }
 
         positions[contig] = (fwd_pos, rev_pos)
 
     return positions
 
 
-def get_ref_pos(aln_info: AlignmentInfo, ref_positions: MotifPositions, 
+def get_ref_pos(aln_info: AlignmentInfo, ref_positions: MotifPositions,
                 window: int) -> Iterator[int]:
     if aln_info.fwd_strand:
         ctg_pos = ref_positions[aln_info.ctg][0]
@@ -50,13 +60,14 @@ def get_ref_pos(aln_info: AlignmentInfo, ref_positions: MotifPositions,
     if aln_info.fwd_strand:
         rng = range(aln_info.r_start + window, aln_info.r_end - window)
     else:
-        rng = range(aln_info.r_end - 1 - window, aln_info.r_start - 1 + window, -1)
+        rng = range(aln_info.r_end - 1 - window, aln_info.r_start - 1 + window,
+                    -1)
     for rel, rpos in enumerate(rng, start=window):
         if rpos in ctg_pos:
             yield rel, rpos
 
 
-def extract_features(read_info: ReadInfo, ref_positions: MotifPositions, 
+def extract_features(read_info: ReadInfo, ref_positions: MotifPositions,
                      aligner: mappy.Aligner, window: int) -> Optional[None]:
 
     seq_to_sig = read_info.get_seq_to_sig()
@@ -81,9 +92,9 @@ def extract_features(read_info: ReadInfo, ref_positions: MotifPositions,
         # sig_end -> Start of the first signal point after example
         sig_end = seq_to_sig[q_end]
 
-        example = Example(read_info.read_id, aln_info.ctg, pos, 
-                            signal[sig_start:sig_end], 
-                            ref_seq[rel-window:rel+window+1])
+        example = Example(read_info.read_id, aln_info.ctg, pos,
+                          signal[sig_start:sig_end],
+                          ref_seq[rel - window:rel + window + 1])
         examples.append(example)
 
     return examples
