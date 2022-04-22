53e22185eda7c6780b39895d76baae56fd220c90
Dominik Stanojevic <dominik.stanojevic@outlook.com>
2022-04-22 05:20:14 +0000

    Bugfixes in convert_labels.py

--- rockfish/rftools/convert_labels.py
+++ rockfish/rftools/convert_labels.py
@@ -9,21 +9,21 @@ from rockfish.rf_format import *
 
 
 def main(args: argparse.Namespace):
+    tqdm.write('Started reading labels')
     labels = DictLabels(args.labels)
 
-    with open(args.data) as rf_src:
+    tqdm.write('Started parsing data')
+    with open(args.data, 'rb') as rf_src, open(args.output, 'wb') as output:
         header = RFHeader.parse_header(rf_src)
 
-        probs = np.empty((header.n_examples,), dtype=np.half)
         for i in trange(header.n_examples):
-            example_header = RFExampleHeader.parse_bytes(rf_src.read(EXAMPLE_HEADER_STRUCT))
+            example_header = RFExampleHeader.parse_bytes(rf_src.read(EXAMPLE_HEADER_STRUCT.size))
             rf_src.seek(example_header.example_len(args.seq_len), 1)
 
             ctg = header.ctgs[example_header.ctg_id]
             prob = labels.get_label(example_header.read_id, ctg, example_header.pos)
-            probs[i] = prob
 
-    np.save(args.output, probs)
+            output.write(np.half(prob).tobytes())
 
 
 def add_convert_labels_args(parser: argparse.ArgumentParser) -> argparse.Namespace:
@@ -31,5 +31,3 @@ def add_convert_labels_args(parser: argparse.ArgumentParser) -> argparse.Namespa
     parser.add_argument('-l', '--labels', type=str, required=True)
     parser.add_argument('-o', '--output', type=str, required=True)
     parser.add_argument('-s', '--seq_len', type=int, default=31)
-
-    return parser.parse_args()
