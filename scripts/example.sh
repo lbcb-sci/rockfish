69f00bcfb327f1144fbb4ce803cf79f0a6572e0a
Dominik Stanojevic <dominik.stanojevic@outlook.com>
2022-11-09 12:58:58 +0800

    Updated README Bugfixes for CPU inference Updated example.sh Added LICENSE

--- scripts/example.sh
+++ scripts/example.sh
@@ -1,16 +1,16 @@
+#!/bin/bash
+
 # Download data from Nanopolish
-wget http://s3.climb.ac.uk/nanopolish_tutorial/methylation_example.tar.gz
-tar -xzf methylation_example.tar.gz
-cd methylation_example
-
-# Run Guppy Basecaller
-$GUPPY_PATH -i fast5_files/ -s basecalled -r --fast5_out -c dna_r9.4.1_450bps_sup.cfg -x "cuda:0"
-
-# Download T2T CHM13 Assembly (v2.0)
-wget https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/analysis_set/chm13v2.0.fa.gz
+wget https://www.dropbox.com/s/4ga4j1diftabn8n/example.tar.gz?dl=0 -O example.tar.gz
+tar -xzf example.tar.gz
+cd example
 
 # Download small model
 rockfish download -m small -s .
 
 # Run inference
-rockfish inference -i basecalled/workspace/ --model_path rf_small.ckpt -d 0 -t 8 --reference chm13v2.0.fa.gz
+if [[ -z ${CUDA_VISIBLE_DEVICES} ]]; then
+  rockfish inference -i fast5/ --model_path rf_small.ckpt -t 2 --reference chm13v2.0_chr20.fa.gz
+else
+  rockfish inference -i fast5/ --model_path rf_small.ckpt -d ${CUDA_VISIBLE_DEVICES} -t 2 --reference chm13v2.0_chr20.fa.gz
+fi
